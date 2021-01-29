6ecc7e293303fa7c2fcc779be2f6bd67f7805741
nafisnihal <nafisnihal12@gmail.com>
2021-01-29 22:17:37 +0600

    First commit

---
+++ FirstTime.cs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FirstTime.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            for(int i=5; i>=0; i--)
+            {
+                for (int k=0; k<=i; k++)
+                {
+                    Console.Write(" ");
+                }
+                name += "*";
+                Console.Write(name);
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
